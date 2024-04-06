930b8e7430d903e0dcb2f4c7381dfafd91323ff3
SyikhaSonOfAdin <syikhasmart@gmail.com>
2024-04-06 14:38:17 +0700

    Update

--- src/.config/.url.js
+++ src/.config/.url.js
@@ -1,4 +1,4 @@
-const URL = "http://localhost:3000"
-// const URLAPI = "https://wppl.syikha.com"
+// const URL = "http://localhost:3000"
+const URLAPI = "https://wppl.syikha.com"
 
 export default URLAPI
